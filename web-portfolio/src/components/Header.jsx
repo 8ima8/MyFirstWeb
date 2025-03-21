3878cba26a7a07d7049272a8f015e6be546ea1c5
8ima8 <fabianbeltra@MacBook-Pro-de-Fabian.local>
2025-03-21 19:30:26 -0400

    Add Header

---
+++ web-portfolio/src/components/Header.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+export function Header(){
+    return (
+        <div>
+            <header>
+                <button>Home</button>
+                <button>Aboute Me</button>
+                <button>Projects</button>
+                <button>Skills</button>
+                <button>Contact</button>
+            </header>
+
+        </div>
+
+    )
+}
+
+
+
+
+// const Home = () => {
+//     return (
+//         <div>
+        
+//         </div>
+//     )
+// }
+
+//export default Home
